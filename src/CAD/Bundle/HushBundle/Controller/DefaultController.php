2b4c0cfad32974323dc6504552c82398c0d4c065
Daniel Bell <me@daniel-bell.co.uk>
2014-04-01 07:34:06 +0100

    Moved registration form to index page

--- src/CAD/Bundle/HushBundle/Controller/DefaultController.php
+++ src/CAD/Bundle/HushBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace CAD\Bundle\HushBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CAD\Bundle\HushBundle\Entity\Users;
+use CAD\Bundle\HushBundle\Form\UsersType;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('HushBundle:Default:index.html.twig', array('name' => $name));
+    	$entity = new Users();
+        $register_form   = $this->createRegisterForm($entity);
+
+        return $this->render('HushBundle:Default:index.html.twig', array('register_form' => $register_form->createView()));
+    }
+
+    private function createRegisterForm(Users $entity)
+    {
+        $form = $this->createForm(new UsersType(), $entity, array(
+            'action' => $this->generateUrl('users_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Register'));
+
+        return $form;
     }
 }
