8fadd465893466cbe047cd7d5ea7efbefe267308
Daniel Bell <me@daniel-bell.co.uk>
2014-04-02 23:49:53 +0100

    oved login form to index and removed SecurityController and login template

--- src/CAD/Bundle/HushBundle/Controller/DefaultController.php
+++ src/CAD/Bundle/HushBundle/Controller/DefaultController.php
@@ -3,13 +3,26 @@
 namespace CAD\Bundle\HushBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 use CAD\Bundle\HushBundle\Entity\Users;
 use CAD\Bundle\HushBundle\Form\UsersType;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $session = $request->getSession();
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
         if($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') ){
             return $this->redirect($this->generateUrl('chat'));
         }
@@ -17,7 +30,10 @@ class DefaultController extends Controller
     	$entity = new Users();
         $register_form   = $this->createRegisterForm($entity);
 
-        return $this->render('HushBundle:Default:index.html.twig', array('register_form' => $register_form->createView()));
+        return $this->render('HushBundle:Default:index.html.twig', array(
+            'register_form' => $register_form->createView(),
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'=> $error));
     }
 
     private function createRegisterForm(Users $entity)
